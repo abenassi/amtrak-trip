3b461336920575681b20420b79981211aa2e6cc4
Agustín Benassi <agusbenassi@gmail.com>
2015-06-01 18:42:38 -0400

    Update map with changes to the trip. Add timezone correction when calculating duration of services.

--- amtrak_geolocalize.py
+++ amtrak_geolocalize.py
@@ -4,8 +4,25 @@
 """
 amtrak_geolocalize
 
-Geolocalize amtrak stations in a json file, add their coordinates, add the_geom
-field with a LineString.
+Geolocalize amtrak stations in a json file and create a line for each service.
+
+This module takes a json file of amtrak services parsed with `amtrak.py` and
+creates two new georreferenced files: one with points (being the stations of
+the trip) and one with lines (being the services between the stations). Both
+files are dumped into geojson (usable with CartoDB and other mapping services)
+and json (easier to read the information if is not to be used for mapping)
+formats.
+
+You should first use `amtrak.py` to parse the itinerary.
+Example:
+    $ python amtrak.py
+
+and then use `amtrak_geolocalize.py` to convert it into something mappable.
+Example:
+    $ python amtrak_geolocalize.py
+
+    import amtrak_geolocalize
+    amtrak_geolocalize.main()
 
 TODO: find amtrak real rail path (shortest path? amtrak routes dataset? after
 trip data?).
@@ -15,16 +32,26 @@ from __future__ import unicode_literals
 import json
 import shapefile
 from fuzzywuzzy import process
+import arrow
+import requests
+import os
 from pprint import pprint
 # from graph import rail_graph
 
 
-def load_json_services(json_file="./json/amtrak-trip.json"):
-    with open(json_file) as f:
+def load_services(file_name="./json/amtrak-trip.json"):
+    """Load a json file with parsed services from an amtrak itinerary."""
+    with open(file_name) as f:
         return json.loads(f.read())
 
 
 def geolocalize_stations(service, shp_file="amtrk_sta/amtrk_sta"):
+    """Add coordinates to each station found in a service.
+
+    Args:
+        service (dict): A parsed amtrak service.
+        shp_file (str): Path to a shapefile of amtrak stations.
+    """
 
     for key, station in service.items():
         if "station" in key:
@@ -32,7 +59,74 @@ def geolocalize_stations(service, shp_file="amtrk_sta/amtrk_sta"):
             service[coord_key] = find_coordinates(station, shp_file)
 
 
+def correct_time_zones(service):
+    """Correct parsed dates with corresponding time zones.
+
+    Uses the coordinates found for every station and the Google Time Zone API
+    to rewrite the dates adding time zone information
+
+    Args:
+        service (dict): A parsed amtrak service.
+    """
+
+    for key, coordinates in service.items():
+        if "coordinates" in key:
+            arrival_or_depart = key.replace("_coordinates", "")
+            date_key = arrival_or_depart + "_date"
+            date = arrow.get(service[date_key])
+            timestamp = date.timestamp
+
+            tzinfo = _get_tz(coordinates, timestamp)
+            dt_tuple = (date.year, date.month, date.day, date.hour,
+                        date.minute)
+
+            service[date_key] = arrow.get(*dt_tuple, tzinfo=tzinfo).isoformat()
+
+
+def _get_tz(coordinates, timestamp):
+    """Get the timezone of a point in a certain time.
+
+    Use the Google Time Zone API to get it.
+
+    Args:
+        coordinates (list): Given coordinates [lon, lat].
+        timestamp (int): Unix timestamp (necessary to retrieve take into
+            account Daylight Saving Time schemes).
+    """
+
+    payload = {"location": ",".join([unicode(i) for i in
+                                     reversed(coordinates)]),
+               "timestamp": timestamp,
+               "key": os.environ["GOOGLE_API_KEY"]}
+    url = "https://maps.googleapis.com/maps/api/timezone/json"
+
+    return requests.get(url, payload).json()["timeZoneId"]
+
+
+def add_duration(service):
+    """Write the duration of a service into an new field."""
+    service["duration"] = _calc_duration(service)
+    return service
+
+
+def _calc_duration(service):
+    """Calculates the duration of a service."""
+    duration = arrow.get(service["arrival_date"]) - \
+        arrow.get(service["departure_date"])
+    return round(duration.total_seconds() / 60 / 60, 1)
+
+
 def find_coordinates(station, shp_file="amtrk_sta/amtrk_sta"):
+    """Find coordinates for a given amtrak station.
+
+    Args:
+        station (str): Amtrak station.
+        shp_file (str): Path to a shapefile of amtrak stations.
+    Returns:
+        list: Coordinates pulled from the amtrak stations shapefile
+            [-122.29068, 37.840679]
+            [lon, lat]
+    """
 
     sf = shapefile.Reader(shp_file)
 
@@ -50,19 +144,23 @@ def find_coordinates(station, shp_file="amtrk_sta/amtrk_sta"):
 
 
 def create_line(service):
+    """Create a line from departure station to arrival station of a service."""
     return _coords_to_line(service["departure_coordinates"],
                            service["arrival_coordinates"])
 
 
 def _coords_to_line(origin, destination):
+    """Create geojson formated line from 2 points."""
     return {"type": "MultiLineString", "coordinates": [[origin, destination]]}
 
 
 def create_point(coordinates):
+    """Create geojson formated point from lon and lat coordiantes."""
     return {"type": "Point", "coordinates": coordinates}
 
 
 def load_amtrak_path(service, graph):
+    """TODO: It will load a real amtrak path for a service."""
     return _find_amtrak_path(service["departure_coordinates"],
                              service["arrival_coordinates"],
                              graph)
@@ -79,14 +177,15 @@ def _find_amtrak_path(origin, destination, graph):
         identified
     """
 
-    id_origin = _get_amtrak_node_id(origin)
-    id_destination = _get_amtrak_node_id(destination)
+    id_origin = _get_node_id(origin)
+    id_destination = _get_node_id(destination)
     path = graph.find_shortest_path(id_origin, id_destination)
 #
     return _path_to_geojson(path)
 
 
-def _get_amtrak_node_id(coordinates):
+def _get_node_id(coordinates):
+    """Find a node id in the US rail_nodes shapefile given some coordinates."""
 
     sf_nodes = shapefile.Reader("rail/rail_nodes")
     points_generator = (shape.points[0] for shape in sf_nodes.iterShapes())
@@ -101,6 +200,22 @@ def _get_amtrak_node_id(coordinates):
 
 
 def _find_aprox_coord(coordinates, points_generator, threshold=0.001):
+    """Find the index of the closest point given some coordinates.
+
+    Iterate a points generator based on a shapefile trying to find the closest
+    point to some coordiantes with a threshold of maximum difference between
+    any of the coordinates (lon or lat).
+
+    Args:
+        coordinates (list): Given coordinates [lon, lat]
+        points_generator (generator): Yields coordinates [lon, lat] from a
+            shapefile.
+        threshold (float): Maximum percentual difference acceptable between any
+            coordinate (lon or lat).
+    Returns:
+        int: Index of the closest point or -1 if none is found within the
+            constraint given by the difference threshold.
+    """
 
     return_index, min_diff = -1, threshold
     for index, coord in enumerate(points_generator):
@@ -112,6 +227,13 @@ def _find_aprox_coord(coordinates, points_generator, threshold=0.001):
 
 
 def _calculate_coord_diff(coord_a, coord_b):
+    """Calculate the percentual difference between two coordinates.
+
+    It will be the maximum after comparing lon and lat coordinates.
+
+    Args:
+        coord_a, coord_b (list): Given coordinates [lon, lat]
+    """
     diff_x = abs(float(coord_a[0]) / coord_b[0] - 1)
     diff_y = abs(float(coord_a[1]) / coord_b[1] - 1)
     # print coord_a, coord_b, diff_x, diff_y
@@ -123,6 +245,13 @@ def _path_to_geojson(path):
 
 
 def to_geojson_format(services):
+    """Convert a services list in a geojson formated dict.
+
+    Args:
+        services (list): Services with a geojson formated 'the_geom' field.
+    Returns:
+        dict: Formated like a geojson file.
+    """
     geojson_dict = {"type": "FeatureCollection",
                     "features": []}
     for service in services:
@@ -135,20 +264,22 @@ def to_geojson_format(services):
     return geojson_dict
 
 
-def save_new_json_services(services,
-                           json_file_name="./json/amtrak-trip-geoloc.json"):
-    with open(json_file_name, "w") as f:
+def write_services_to_json(services,
+                           file_name="./json/amtrak-trip-geoloc.json"):
+    with open(file_name, "w") as f:
         f.write(json.dumps(services, indent=4))
 
 
 def main():
-    services = load_json_services()
+    services = load_services()
 
     points_dict = {}
     for service in services:
 
         # create lines dict
         geolocalize_stations(service)
+        correct_time_zones(service)
+        add_duration(service)
         service["the_geom"] = create_line(service)
         # service["the_geom"] = load_amtrak_path(service)
 
@@ -184,17 +315,17 @@ def main():
 
     # create points json and geojson files
     # pprint(points_dict)
-    save_new_json_services(points_dict.values(),
+    write_services_to_json(points_dict.values(),
                            "./json/amtrak-trip-points.json")
     geojson_dict = to_geojson_format(points_dict.values())
-    save_new_json_services(geojson_dict,
+    write_services_to_json(geojson_dict,
                            "./geojson/amtrak-trip-points.geojson")
 
     # create lines json and geojson files
-    save_new_json_services(services, "./json/amtrak-trip-lines.json")
+    write_services_to_json(services, "./json/amtrak-trip-lines.json")
     # pprint(services)
     geojson_dict = to_geojson_format(services)
-    save_new_json_services(geojson_dict, "./geojson/amtrak-trip-lines.geojson")
+    write_services_to_json(geojson_dict, "./geojson/amtrak-trip-lines.geojson")
 
 
 if __name__ == '__main__':
